14587146c6eab01d32c7e6282e265feb0ecd632a
KDSaltyPony <kallofdragon@gmail.com>
2021-04-21 11:39:15 +0200

    Service provider

---
+++ src/Providers/LumenServiceProvider.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * LumenServiceProvider class file
+ * 
+ * PHP Version 7.2.19
+ * 
+ * @category Controller
+ * @package  LumePack\Foundation\Providers
+ * @author   KDSaltyPony <kallofdragon@gmail.com>
+ * @license  https://opensource.org/licenses/gpl-3.0.html GNU Public License
+ * @link     none
+ */
+namespace LumePack\Foundation\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * LumenServiceProvider
+ * 
+ * @category Service
+ * @package  LumePack\Foundation\Providers
+ * @author   KDSaltyPony <kallofdragon@gmail.com>
+ * @license  https://opensource.org/licenses/gpl-3.0.html GNU Public License
+ * @link     none
+ */
+class LumenServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Boot the authentication services for the application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+}
