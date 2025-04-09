44de33ffc67596dd5d5be402a8163792594c0596
ljaeger <ljaeger@diatem.net>
2025-04-09 11:35:14 +0200

    evolutions & patch gd

--- src/Data/Models/Storage/File.php
+++ src/Data/Models/Storage/File.php
@@ -212,7 +212,7 @@ class File extends BaseModel
                     $old_width = imagesx($source);
                     $old_height = imagesy($source);
                     $new_ratio = $this->width / $this->height;
-                    $old_ratio = $width / $height;
+                    $old_ratio = $old_width / $old_width;
 
                     if ($old_ratio > $new_ratio) {
                         $new_width =  $this->width;
