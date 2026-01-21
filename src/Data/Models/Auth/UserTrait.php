41273080b9b05b954d37913edd7c87938cdd14d4
ljaeger <ljaeger@diatem.net>
2026-01-21 16:59:56 +0100

    patch disable rehash

--- src/Data/Models/Auth/UserTrait.php
+++ src/Data/Models/Auth/UserTrait.php
@@ -55,9 +55,9 @@ trait UserTrait
                 $model->email_token = User::emailTokenize();
             }
 
-            if (!is_null($model->password) && Hash::needsRehash($model->password)) {
-                $model->password = Hash::make($model->password);
-            }
+            // if (!is_null($model->password) && Hash::needsRehash($model->password)) {
+            //     $model->password = Hash::make($model->password);
+            // }
         });
 
         static::saved(function (User $model) {
